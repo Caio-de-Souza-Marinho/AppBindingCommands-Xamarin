9bff982d4e552cb2721986eae5e77b56f757b2ae
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-08-09 19:56:03 -0300

    Modificando eventos do ciclo de vida do app

--- AppBindingCommands/AppBindingCommands/App.xaml.cs
+++ AppBindingCommands/AppBindingCommands/App.xaml.cs
@@ -10,19 +10,28 @@ namespace AppBindingCommands
         {
             InitializeComponent();
 
+            DateTime data = DateTime.Now;
+            Application.Current.Properties["dtAtual"] = data;
+            Application.Current.Properties["AcaoInicial"] = string.Format(" * App executado às {0}. \n", data);
+
             MainPage = new MainPage();
         }
 
         protected override void OnStart()
         {
+            Application.Current.Properties["AcaoStart"] = string.Format(" * App iniciado às {0}. \n", DateTime.Now);
         }
 
         protected override void OnSleep()
         {
+            Application.Current.Properties["AcaoSleep"] = string.Format(" * App em segundo plano às {0}. \n", DateTime.Now);
+
         }
 
         protected override void OnResume()
         {
+            Application.Current.Properties["AcaoResume"] = string.Format(" * App reativado às {0}. \n", DateTime.Now);
+
         }
     }
 }
